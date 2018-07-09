97d2821d3fc5a85e9fff39802d3ff653f8917fbb
Yeondam Park <giyen9088@gmail.com>
2018-07-08 22:11:44 -0700

    connecting DB(MongoDB)

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config/dev");
+
+mongoose.connect(
+  config.DB_URI,
+  { useNewUrlParser: true }
+);
+
+const app = express();
+
+app.get("/rentals", (req, res) => {
+  res.json({ success: true });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
