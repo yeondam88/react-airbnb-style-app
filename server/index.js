ffa053e0f275761de78e20c30dfa9a2653497b52
Yeondam Park <giyen9088@gmail.com>
2018-07-08 23:00:42 -0700

    adding fake data to database

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("./config/dev");
+const FakeDb = require("./fake-db");
 const Rental = require("./models/rental");
 
-mongoose.connect(
-  config.DB_URI,
-  { useNewUrlParser: true }
-);
+const rentalRoutes = require("./routes/rentals");
+
+mongoose
+  .connect(
+    config.DB_URI,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    const fakeDb = new FakeDb();
+    fakeDb.seedDb();
+  });
 
 const app = express();
+
+app.use("/api/v1/rentals", rentalRoutes);
 
 app.get("/rentals", (req, res) => {
   res.json({ success: true });
