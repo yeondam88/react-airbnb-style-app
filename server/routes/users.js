42dc7947901dff1b698f4c9012b6dccc1bf973c7
Yeondam Park <giyen9088@gmail.com>
2018-07-09 22:59:51 -0700

    adding users route

---
+++ server/routes/users.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/auth", (req, res) => {});
+
+router.post("/register", (req, res) => {});
+
+module.exports = router;
