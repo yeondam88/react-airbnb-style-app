ef81e31cd7ef8172d2a19f82a11fed1e79baf0a4
Yeondam Park <giyen9088@gmail.com>
2018-07-10 23:36:58 -0700

    working on register

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,62 @@
+const User = require("../models/user");
+
 exports.auth = (req, res) => {};
 
 exports.register = (req, res) => {
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
-  const passwordConfirmation = req.body.passwordConfirmation;
+  const { username, email, password, passwordConfirmation } = req.body;
+
+  if (!password || !email) {
+    return res.status(422).send({
+      errors: [
+        { title: "Data Missing!", detail: "Provide email and password." }
+      ]
+    });
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.status(422).send({
+      errors: [
+        { title: "Invalid Password!", detail: "Password is not matched!." }
+      ]
+    });
+  }
+
+  User.findOne(
+    {
+      email
+    },
+    (err, existingUser) => {
+      if (err) {
+        return res.status(422).send({
+          mongoose: "handle mongoose errors in next"
+        });
+      }
+      if (existingUser) {
+        return res.status(422).send({
+          errors: [
+            {
+              title: "Invalid Email",
+              detail: "User with this email already exist!"
+            }
+          ]
+        });
+      }
+
+      const user = new User({
+        username,
+        email,
+        password
+      });
+
+      user.save(err => {
+        if (err) {
+          return res.status(422).send("mmmm");
+        }
+
+        return res.json({ resgister: true });
+      });
+    }
+  );
 
   res.json({
     username,
