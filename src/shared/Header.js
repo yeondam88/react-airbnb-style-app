fdae4c207a56b87773e368d626ae3ac025096359
Yeondam Park <giyen9088@gmail.com>
2018-07-16 22:32:07 -0700

    adding header with style

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,5 +1,48 @@
 import React, { Component } from "react";
 
-const Header = () => <h1>Header!</h1>;
+const Header = () => (
+  <nav className="navbar navbar-dark navbar-expand-lg">
+    <div className="container">
+      <a href="" className="navbar-brand">
+        BookWithMe
+      </a>
+      <form action="" className="form-inline my-2 my-lg-0">
+        <input
+          type="search"
+          className="form-control mr-sm-2 bwm-search"
+          placeholder="Try 'New York'"
+          aria-label="Search"
+        />
+        <button
+          className="btn btn-outline-success my-2 my-sm-0 btn-bwm-search"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav ml-auto">
+          <a href="" className="nav-item nav-link active">
+            Login <span className="sr-only">(current)</span>
+          </a>
+          <a href="" className="nav-item nav-link">
+            Register
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
+);
 
 export default Header;
