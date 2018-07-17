5227ac774e18e223aa11a1fecec590f745b213e4
Yeondam Park <giyen9088@gmail.com>
2018-07-16 22:51:03 -0700

    adding rental card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import Header from "./shared/Header";
+import RentalCard from "./components/rental/RentalCard";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />I am app
+        <Header />
+        <div className="container">
+          <section id="rentalListing">
+            <h1 className="page-title">Your Home All Around the World</h1>
+            <div className="row">
+              <RentalCard />
+              <RentalCard />
+              <RentalCard />
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
---
+++ src/components/rental/RentalCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const RentalCard = props => (
+  <div className="col-md-3 col-xs-6">
+    <div className="card bwm-card">
+      <img
+        src="http://via.placeholder.com/350x250"
+        alt=""
+        className="card-img-top"
+      />
+      <div className="card-block">
+        <h6 className="card-subtitle">Whole Apartment &#183; New York</h6>
+        <h4 className="card-title">Some nice apartment</h4>
+        <p className="card-text">$240 per night &#183; Free Cancelation</p>
+        <a href="" className="card-link">
+          More Info
+        </a>
+      </div>
+    </div>
+  </div>
+);
+
+export default RentalCard;
