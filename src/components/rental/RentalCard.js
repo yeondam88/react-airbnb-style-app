785b462ea6e60f75ff6f352113bcfde5f54ae319
Yeondam Park <giyen9088@gmail.com>
2018-07-18 22:36:21 -0700

    linking rentalCard

--- src/components/rental/RentalCard.js
+++ src/components/rental/RentalCard.js
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RentalCard = ({ colNum, rental }) => (
   <div className={colNum}>
-    <div className="card bwm-card">
-      <img src={rental.image} alt="" className="card-img-top" />
-      <div className="card-block">
-        <h6 className="card-subtitle">
-          {rental.shared ? "shared" : "whole"} {rental.category} &#183;{" "}
-          {rental.city}
-        </h6>
-        <h4 className="card-title">{rental.title}</h4>
-        <p className="card-text">
-          ${rental.dailyRate} per night &#183; Free Cancelation
-        </p>
-        <a href="" className="card-link">
-          More Info
-        </a>
+    <Link to={`/rentals/${rental.id}`}>
+      <div className="card bwm-card">
+        <img src={rental.image} alt="" className="card-img-top" />
+        <div className="card-block">
+          <h6 className="card-subtitle">
+            {rental.shared ? "shared" : "whole"} {rental.category} &#183;{" "}
+            {rental.city}
+          </h6>
+          <h4 className="card-title">{rental.title}</h4>
+          <p className="card-text">
+            ${rental.dailyRate} per night &#183; Free Cancelation
+          </p>
+          <a href="" className="card-link">
+            More Info
+          </a>
+        </div>
       </div>
-    </div>
+    </Link>
   </div>
 );
 
