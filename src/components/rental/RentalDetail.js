4a020665ad6cac0515d5c11c24f2988c0c54bb15
Yeondam Park <giyen9088@gmail.com>
2018-07-20 23:24:58 -0700

    add loading text when data is not available

--- src/components/rental/RentalDetail.js
+++ src/components/rental/RentalDetail.js
@@ -10,15 +10,19 @@ class RentalDetail extends Component {
 
   render() {
     const rental = this.props.rental;
-    return (
-      <div>
-        <h1>Rental Detail!!!</h1>
-        <p>{rental.title}</p>
-        <p>{rental.city}</p>
-        <p>{rental.description}</p>
-        <p>per night ${rental.dailyRate}</p>
-      </div>
-    );
+    if (rental.id) {
+      return (
+        <div>
+          <h1>Rental Detail!!!</h1>
+          <p>{rental.title}</p>
+          <p>{rental.city}</p>
+          <p>{rental.description}</p>
+          <p>per night ${rental.dailyRate}</p>
+        </div>
+      );
+    } else {
+      return <h1>loading...</h1>;
+    }
   }
 }
 
