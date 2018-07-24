273ea32adeb171e3651622e708a99acc4f29ee00
Yeondam Park <giyen9088@gmail.com>
2018-07-23 22:12:57 -0700

    adding style and font-awesome library

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -12,13 +12,74 @@ class RentalDetail extends Component {
     const { rental } = this.props;
     if (rental._id) {
       return (
-        <div>
-          <h1>Rental Detail!!!</h1>
-          <p>{rental.title}</p>
-          <p>{rental.city}</p>
-          <p>{rental.description}</p>
-          <p>per night ${rental.dailyRate}</p>
-        </div>
+        <section id="rentalDetails">
+          <div className="upper-section">
+            <div className="row">
+              <div className="col-md-6">
+                <img src={rental.image} alt="" />
+              </div>
+              <div className="col-md-6">
+                <img src={rental.image} alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className="details-section">
+            <div className="row">
+              <div className="col-md-8">
+                <div className="rental">
+                  <h2 className={`rental-type ${rental.category}`}>
+                    {rental.shared} {rental.category}
+                  </h2>
+                  <h1 className="rental-title">{rental.title}</h1>
+                  <h2 className="rental-city">{rental.city}</h2>
+                  <div className="rental-room-info">
+                    <span>
+                      <i className="fa fa-building" />
+                      {rental.bedrooms} bedrooms
+                    </span>
+                    <span>
+                      <i className="fa fa-user" /> {rental.bedrooms + 4} guests
+                    </span>
+                    <span>
+                      <i className="fa fa-bed" /> {rental.bedrooms + 2} beds
+                    </span>
+                  </div>
+                  <p className="rental-description">{rental.description}</p>
+                  <hr />
+                  <div className="rental-assets">
+                    <h3 className="title">Assets</h3>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <span>
+                          <i className="fa fa-asterisk" /> Cooling
+                        </span>
+                        <span>
+                          <i className="fa fa-thermometer" /> Heating
+                        </span>
+                        <span>
+                          <i className="fa fa-location-arrow" /> Iron
+                        </span>
+                      </div>
+                      <div className="col-md-6">
+                        <span>
+                          <i className="fa fa-desktop" /> Working area
+                        </span>
+                        <span>
+                          <i className="fa fa-cube" /> Washing machine
+                        </span>
+                        <span>
+                          <i className="fa fa-cube" /> Dishwasher
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-4"> BOOKING</div>
+            </div>
+          </div>
+        </section>
       );
     } else {
       return <h1>loading...</h1>;
