afd8b8e0d932f832604021758dc0bc532fe797ba
Yeondam Park <giyen9088@gmail.com>
2018-08-01 23:31:38 -0700

    showing error message when it over limit allowed guests number

--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -2,17 +2,22 @@ import React from "react";
 import DateRangePicker from "react-bootstrap-daterangepicker";
 import { getRangeOfDates } from "helpers";
 import * as moment from "moment";
+import { throws } from "assert";
 
 class Booking extends React.Component {
-  constructor() {
-    super();
+  state = {
+    startAt: "",
+    endAt: "",
+    guests: 0,
+    errors: "",
+    isErrors: false
+  };
+  bookedOutDates = [];
+  dateRef = React.createRef();
 
-    this.bookedOutDates = [];
-  }
-
-  componentWillMount() {
+  componentWillMount = () => {
     this.getBookedOutDates();
-  }
+  };
 
   getBookedOutDates = () => {
     const { bookings } = this.props.rental;
@@ -36,6 +41,43 @@ class Booking extends React.Component {
     );
   };
 
+  handleApply = (event, picker) => {
+    const startAt = picker.startDate.format("Y/MM/DD");
+    const endAt = picker.endDate.format("Y/MM/DD");
+
+    this.dateRef.current.value = `${startAt} to ${endAt}`;
+
+    this.setState({
+      startAt,
+      endAt
+    });
+  };
+
+  selectGuests = event => {
+    const { guests } = this.state;
+    const { rental } = this.props;
+
+    this.setState({
+      guests: parseInt(event.target.value)
+    });
+
+    if (guests >= rental.bedrooms + 4) {
+      this.setState({
+        errors: `You can not have more than ${rental.bedrooms + 4} people.`,
+        isErrors: true
+      });
+    } else {
+      this.setState({
+        errors: "",
+        isErrors: false
+      });
+    }
+  };
+
+  handleReserveButton = () => {
+    console.log(this.state);
+  };
+
   render() {
     const { rental } = this.props;
 
@@ -49,24 +91,39 @@ class Booking extends React.Component {
         <div className="form-group">
           <label htmlFor="dates">Dates</label>
           <DateRangePicker
+            onApply={this.handleApply}
             isInvalidDate={this.checkInvalidDates}
             opens="left"
             containerStyles={{ display: "block" }}
           >
-            <input id="dates" type="text" className="form-control" />
+            <input
+              ref={this.dateRef}
+              id="dates"
+              type="text"
+              className="form-control"
+            />
           </DateRangePicker>
         </div>
         <div className="form-group">
           <label htmlFor="guests">Guests</label>
           <input
+            onChange={this.selectGuests}
+            value={this.state.guests}
             type="number"
             className="form-control"
             id="guests"
-            aria-describedby="emailHelp"
+            aria-describedby="guests"
             placeholder=""
           />
+          {this.state.errors ? (
+            <p className="alert alert-danger">{this.state.errors}</p>
+          ) : null}
         </div>
-        <button className="btn btn-bwm btn-confirm btn-block">
+        <button
+          disabled={this.state.isErrors}
+          onClick={this.handleReserveButton}
+          className="btn btn-bwm btn-confirm btn-block"
+        >
           Reserve place now
         </button>
         <hr />
