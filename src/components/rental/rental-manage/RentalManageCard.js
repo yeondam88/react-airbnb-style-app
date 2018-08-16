064e18419e18dae4b1a256341ed8cc43c956bd26
Yeondam Park <giyen9088@gmail.com>
2018-08-15 21:37:44 -0700

    adding rental delete button

--- src/components/rental/rental-manage/RentalManageCard.js
+++ src/components/rental/rental-manage/RentalManageCard.js
@@ -4,11 +4,31 @@ import { toUpperCase, pretifyDate } from "helpers";
 import RentalManageModal from "./RentalManageModal";
 
 class RentalManageCard extends Component {
+  state = {
+    wantDelete: false
+  };
+
+  showDeleteMenu = () => {
+    this.setState({
+      wantDelete: true
+    });
+  };
+
+  closeDeleteMenu = () => {
+    this.setState({
+      wantDelete: false
+    });
+  };
+
   render() {
     const { image, title, city, _id, createdAt, bookings } = this.props.rental;
+    const { wantDelete } = this.state;
+
+    const deleteClass = wantDelete ? "toBeDeleted" : "";
+
     return (
       <div key={_id} className="col-md-4 rental-card">
-        <div className="card">
+        <div className={`card text-center ${deleteClass}`}>
           <img className="card-img-top" src={image} alt="Card image cap" />
           <div className="card-body">
             <p className="card-text">
@@ -25,7 +45,27 @@ class RentalManageCard extends Component {
               bookings.length > 0 && <RentalManageModal bookings={bookings} />}
           </div>
           <div className="card-footer">
-            <p className="card-text">Created at {pretifyDate(createdAt)}</p>
+            Created at {pretifyDate(createdAt)}
+            {!wantDelete && (
+              <button
+                onClick={this.showDeleteMenu}
+                className="btn btn-sm btn-danger m-2"
+              >
+                Delete
+              </button>
+            )}
+            {wantDelete && (
+              <div>
+                Are you sure ?
+                <button className="btn btn-sm btn-danger m-2">Yes</button>
+                <button
+                  onClick={this.closeDeleteMenu}
+                  className="btn btn-sm btn-success"
+                >
+                  No
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
